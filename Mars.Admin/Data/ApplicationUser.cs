6302ca77b1819be221adec4c16262c94bd039d63
Reza Moghbel <r.moghbel@hotmail.com>
2025-09-16 15:52:53 +0100

    Initial commit

---
+++ Mars.Admin/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Mars.Admin.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
